1e9be40094a719bcbd995f4e71d1789b9edd60ae
David Ruyle <djr.taureau@gmail.com>
2018-07-16 10:40:49 -0500

    initial commit

---
+++ src/app/static/features/features.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+import { environment as env } from '@env/environment';
+import { ROUTE_ANIMATIONS_ELEMENTS } from '@app/core';
+
+@Component({
+  selector: 'app-features',
+  templateUrl: './features.component.html',
+  styleUrls: ['./features.component.scss']
+})
+
+export class FeaturesComponent implements OnInit {
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
+  versions = env.versions;
+  public projects = PROJECTS;
+
+  ngOnInit() {}
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
+}
+
+const PROJECTS: any[] = [
+  {'title': 'Pomodoit-ngrx',
+   'img': '../../../assets/pomodoist.png',
+   'github': '',
+   'stackblitz': '',
+   'deployed': ''
+  },
+  {'title': 'BlocJams-angular',
+  'img': '../../../assets/pomodoist.png',
+  'github': '',
+  'stackblitz': '',
+  'deployed': ''
+ },
+ {'title': 'BlocClack',
+ 'img': '../../../assets/pomodoist.png',
+ 'github': '',
+ 'stackblitz': '',
+ 'deployed': ''
+},
+{'title': 'BlocJams-React',
+'img': '../../../assets/pomodoist.png',
+'github': '',
+'stackblitz': '',
+'deployed': ''
+},
+{'title': 'Blocipedia',
+'img': '../../../assets/pomodoist.png',
+'github': '',
+'stackblitz': '',
+'deployed': ''
+},
+{'title': 'Kele API Client',
+'img': '../../../assets/pomodoist.png',
+'github': '',
+'stackblitz': '',
+'deployed': ''
+},
+{'title': 'Pomodoist-Assistant',
+'img': '../../../assets/pomodoist.png',
+'github': '',
+'stackblitz': '',
+'deployed': ''
+}
+];
